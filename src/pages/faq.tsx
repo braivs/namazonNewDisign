d19a82070e68ceacb908b04d157ddf1347102d18
Briws <brightwiths@gmail.com>
2024-06-25 15:21:00 +0700

    Convert to typescript: installed dependencies, renamed files to TS. Not compiled at this step.

---
+++ src/pages/faq.tsx
@@ -0,0 +1,15 @@
+import Faq from '@/components/faq/faq';
+import SEO from '@/components/seo';
+import Wrapper from '@/layout/wrapper';
+import React from 'react';
+
+const index = () => {
+    return (
+        <Wrapper>
+            <SEO pageTitle="Faq " />
+            <FAQ />
+        </Wrapper>
+    );
+};
+
+export default index;
