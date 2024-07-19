fe8da59d11a793a3a20041195b2dc10c2bc84c81
Briws <brightwiths@gmail.com>
2024-07-20 00:44:05 +0700

    fixed active tab highlight for NavMenu

--- src/layout/header/nav-menu.tsx
+++ src/layout/header/nav-menu.tsx
@@ -1,15 +1,18 @@
-import Link from "next/link.js";
+import Link from "next/link";
 import React from "react";
-import menu_data from "./menu-data.ts";
+import { useRouter } from "next/router";
+import menu_data from "@/layout/header/menu-data";
 
-const NavMenu = ({num=false}) => {
+const NavMenu = ({ num = false }) => {
+  const { asPath } = useRouter(); // Get the current path
+
   return (
     <>
       <ul>
         {menu_data.map((menu, index) =>
           menu.has_dropdown ? (
-            <li key={menu.id} className="has-dropdown">
-              <Link className={`${menu?.active}`} href={menu.link}>
+            <li key={menu.id} className={`has-dropdown ${asPath === menu.link ? 'active' : ''}`}>
+              <Link className={`${asPath === menu.link ? 'active' : ''}`} href={menu.link}>
                 {num && index <= 9
                   ? `0${index + 1 + "."}`
                   : num && index + 1 + "."}
@@ -17,17 +20,19 @@ const NavMenu = ({num=false}) => {
               </Link>
               {menu.has_dropdown && (
                 <ul className="sub-menu">
-                  {menu.sub_menus.map((sub_m, i) => (
+                  {menu.sub_menus?.map((sub_m, i) => (
                     <li key={i}>
-                      <Link href={sub_m.link}>{sub_m.title}</Link>
+                      <Link className={`${asPath === sub_m.link ? 'active' : ''}`} href={sub_m.link}>
+                        {sub_m.title}
+                      </Link>
                     </li>
                   ))}
                 </ul>
               )}
             </li>
           ) : (
-            <li key={menu.id}>
-              <Link href={menu.link}>
+            <li key={menu.id} className={asPath === menu.link ? 'active' : ''}>
+              <Link className={asPath === menu.link ? 'active' : ''} href={menu.link}>
                 {num && index <= 9
                   ? `0${index + 1 + "."}`
                   : num && index + 1 + "."}
