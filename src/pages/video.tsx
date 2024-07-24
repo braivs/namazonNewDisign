82f2106598b40e8a9535bb9487aad06b729e2455
Briws <brightwiths@gmail.com>
2024-07-25 02:55:39 +0700

    Refactor video component paths
    Updated import paths for video components to reflect new structure, enhancing code organization. Renamed related files from 'research' to 'video'.

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -1,4 +1,4 @@
-import Video from "@/components/research/video";
+import Video from "@/components/video/video";
 import SEO from "@/components/seo";
 import Wrapper from "@/layout/wrapper";
 import React from "react";
