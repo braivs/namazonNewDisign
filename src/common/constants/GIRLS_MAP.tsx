4b66cd26554765c39271c6bdc497a65c8ead72bf
Briws <brightwiths@gmail.com>
2024-07-27 05:49:17 +0700

    Girls: added elena_vasileva2013

--- src/common/constants/GIRLS_MAP.tsx
+++ src/common/constants/GIRLS_MAP.tsx
@@ -1,9 +1,11 @@
 import {FC} from "react"
 import {tais2013Desc} from "@/data/girls-description/tais2013Desc"
 import {kara_tellerDesc} from "@/data/girls-description/kara_tellerDesc"
+import {ElenaVasileva2013Desc} from "@/data/girls-description/elena_vasileva2013Desc"
 
 export const GIRLS_MAP: Record<string, FC> = {
     'tais2013': tais2013Desc,
     'kara_teller': kara_tellerDesc,
+    'elena_vasileva2013': ElenaVasileva2013Desc,
 };
 
