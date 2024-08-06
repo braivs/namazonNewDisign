24d2d081436a37acc0be418252687cff328cb3b3
Briws <brightwiths@gmail.com>
2024-08-06 21:06:53 +0700

    added TS to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import "../styles/index.scss";
 import "../../node_modules/yet-another-react-lightbox/dist/styles.css";
+import {FC} from "react"
+
+type AppProps = {
+  Component: FC
+  pageProps: any
+}
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
