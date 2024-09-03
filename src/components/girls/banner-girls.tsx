da2793d8fa7c9baf1912dd7957e72073595f3a99
Briws <brightwiths@gmail.com>
2024-09-04 00:52:36 +0700

    made minimal adaptive for banner-girls

--- src/components/girls/banner-girls.tsx
+++ src/components/girls/banner-girls.tsx
@@ -7,12 +7,12 @@ import cn from "classnames"
 
 const BannerGirls = () => {
   const dispatch = useAppDispatch()
-  
+
   const onAll = () => dispatch(girlsActions.setCategory('ALL'))
   const on2010_2011 = () => dispatch(girlsActions.setCategory('2010 - 2011'))
   const on2012_2013 = () => dispatch(girlsActions.setCategory('2012 - 2013'))
 
-  const selected_category = useAppSelector(GirlsSelected_categorySelector )
+  const selected_category = useAppSelector(GirlsSelected_categorySelector)
 
   return (
     <>
@@ -25,18 +25,27 @@ const BannerGirls = () => {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-xl-7 col-lg-12 col-md-12 col-12">
-              <div className="tp-breadcrumb">
-                <h2 className="tp-breadcrumb__title">Girls :</h2>
-                <div className={cn(selected_category === 'ALL' && sC.selectedCategory, "tp-breadcrumb__element")} onClick={onAll}>ALL</div>
-                <div className={cn(selected_category === '2010 - 2011' && sC.selectedCategory, "tp-breadcrumb__element")} onClick={on2010_2011}>2010 - 2011</div>
-                <div className={cn(selected_category === '2012 - 2013' && sC.selectedCategory, "tp-breadcrumb__element")} onClick={on2012_2013}>2012 - 2013</div>
+              <div className={sC.tpBreadcrumb}>
+                <h2 className={sC.title}>Girls :</h2>
+                <div className={sC.group}>
+                  <div className={cn(sC.element, selected_category === 'ALL' && sC.selectedCategory)}
+                       onClick={onAll}>ALL
+                  </div>
+                  <div className={cn(sC.element, selected_category === '2010 - 2011' && sC.selectedCategory)}
+                       onClick={on2010_2011}>2010 - 2011
+                  </div>
+                  <div className={cn(sC.element, selected_category === '2012 - 2013' && sC.selectedCategory)}
+                       onClick={on2012_2013}>2012 - 2013
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default BannerGirls;
+export default BannerGirls
