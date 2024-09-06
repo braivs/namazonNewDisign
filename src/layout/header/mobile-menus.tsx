3cfe8c2687b268c137e95c65302d0591ed9af230
Briws <brightwiths@gmail.com>
2024-09-06 22:28:32 +0700

    Removed submenu from mobile menu. Added sharp package..

--- src/layout/header/mobile-menus.tsx
+++ src/layout/header/mobile-menus.tsx
@@ -28,11 +28,6 @@ const MobileMenus = () => {
                       display: navTitle === menu.title ? "block" : "none",
                     }}
                   >
-                    {menu.sub_menus?.map((sub, i) => (
-                      <li key={i}>
-                        <Link href={sub.link}>{sub.title}</Link>
-                      </li>
-                    ))}
                   </ul>
                   <a
                     className={`mean-expand ${
