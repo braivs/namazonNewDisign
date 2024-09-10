e35c945213f268c93c0ce4fd84b6cb40ed5b09eb
Briws <brightwiths@gmail.com>
2024-09-10 23:33:28 +0700

    added json lock file to git, because gh actions requires it

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'export',
+  basePath: '/namazonNewDisign',
+  assetPrefix: '/namazonNewDisign/',
   images: {
     unoptimized: true,
   },
