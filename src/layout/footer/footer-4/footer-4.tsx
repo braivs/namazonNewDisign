253a91968fd658024ce07c7ace3c385b6f30dc67
Braivs <braivs@proton.me>
2026-05-20 20:58:29 +0700

    Add link to Mixed Wrestling Forum

--- src/layout/footer/footer-4/footer-4.tsx
+++ src/layout/footer/footer-4/footer-4.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 import s from './footer-4.module.scss'
 import {SocialIcons} from "@/components/social-icons/SocialIcons"
 
-const FooterFour = ({ style_2 = false }) => {
+const FooterFour = ({style_2 = false}) => {
   return (
     <div className={`footer-area ${style_2 ? 'pt-105' : 'tp-footer-white-content theme-bg pt-95'}`}>
       <div>
@@ -37,6 +37,9 @@ const FooterFour = ({ style_2 = false }) => {
                     <li>
                       <Link href="/contact">Contact</Link>
                     </li>
+                    <li>
+                      <Link href="https://www.malevsfemale.org/">Mixed Wrestling Forum</Link>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -44,14 +47,14 @@ const FooterFour = ({ style_2 = false }) => {
             <Col xl={3} lg={3} md={6}>
               <div className="tp-footer-widget footer-2-col-4 mb-40">
                 <span className="tp-footer-widget__title mb-15">Contact Info</span>
-                <SocialIcons />
+                <SocialIcons/>
               </div>
             </Col>
           </Row>
         </Container>
       </div>
       <div className="footer-area-bottom fw-border">
-      <Container>
+        <Container>
           <Row>
             <Col xl={6} lg={6} md={12} xs={12}>
               <div className="footer-widget__copyright copyright-white">
