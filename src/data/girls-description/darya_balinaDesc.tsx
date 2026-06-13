f31ed1f9b7307ecf6236797f7df93e86c8949201
Braivs <braivs@proton.me>
2026-06-13 23:31:46 +0700

    Add 'girls/kara_teller, girls/elena_vasileva2013, girls/darya_balina' ru translation

--- src/data/girls-description/darya_balinaDesc.tsx
+++ src/data/girls-description/darya_balinaDesc.tsx
@@ -1,43 +1,66 @@
+'use client'
+
 import imgMain from 'public/assets/img/videoGallery//21/mma-pervaya-nevskaya-arena-2012-2/37.jpg'
 import sC from '@/common/styles.module.scss'
-import Image from "next/image"
-import ButtonGallery from "@/common/ButtonGallery/ButtonGallery"
-import {DIR_MMA_SPORT_HOLIDAY_1, DIR_NEVSKAYA_ARENA_2} from "@/common/constants/ImageContexts"
-import Link from "next/link"
+import Image from 'next/image'
+import ButtonGallery from '@/common/ButtonGallery/ButtonGallery'
+import {DIR_MMA_SPORT_HOLIDAY_1, DIR_NEVSKAYA_ARENA_2} from '@/common/constants/ImageContexts'
+import Link from 'next/link'
+import {Trans, useTranslation} from 'react-i18next'
+
+const transComponents = {
+  b: <b />,
+  br: <br />,
+  linkKara: <Link href="/girls/kara_teller" />,
+  linkPatreon: (
+    <a href="https://www.patreon.com/namazon/shop" target="_blank" rel="noopener noreferrer" />
+  ),
+}
 
 export const DaryaBalinaDesc = () => {
-  const buttonText = 'See photos'
+  const {t} = useTranslation('girls')
+  const ns = 'descriptions.darya_balina'
+  const buttonText = t(`${ns}.seePhotos`, {defaultValue: 'See photos'})
 
   return (
     <>
       <Image className={sC.MainImg} src={imgMain} alt="imgMain"/>
       <p>
-        <b>Darya Balina</b> is a young fighter from St. Petersburg (5’4” / 124 lb). She trains in martial arts at the
-        female self-defense club "Panther". At the Namazon Club, she had a pair of hard-fought MMA fights where she
-        displayed stamina and a will to win. In her last fight there, Darya faced an opponent who exceeded her weight by
-        about 15 pounds. Despite this disadvantage, she was able to avoid defeat.
+        <Trans
+          i18nKey={`${ns}.intro`}
+          ns="girls"
+          components={transComponents}
+          defaults={`<b>Darya Balina</b> is a young fighter from St. Petersburg (5'4" / 124 lb). She trains in martial arts at the female self-defense club "Panther". At the Namazon Club, she had a pair of hard-fought MMA fights where she displayed stamina and a will to win. In her last fight there, Darya faced an opponent who exceeded her weight by about 15 pounds. Despite this disadvantage, she was able to avoid defeat.`}
+        />
       </p>
       <p>
-        The first MMA fight that Darya Balina had at the Namazon Club was against our guest from Moscow, <Link
-        href="/girls/kara_teller">Kara Teller</Link> (5’7” / 123 lb). It went two rounds with the more
-        experienced Kara prevailing, winning by a choke in the middle of the second round.
+        <Trans
+          i18nKey={`${ns}.fight1`}
+          ns="girls"
+          components={transComponents}
+          defaults={`The first MMA fight that Darya Balina had at the Namazon Club was against our guest from Moscow, <linkKara>Kara Teller</linkKara> (5'7" / 123 lb). It went two rounds with the more experienced Kara prevailing, winning by a choke in the middle of the second round.`}
+        />
       </p>
       <ButtonGallery images={DIR_MMA_SPORT_HOLIDAY_1} buttonName={buttonText}/>
       <p>
-        The second time Darya fought in MMA was in July 2012 when she participated in the Nevskaya Arena Tournament
-        organized by the club. In that fight, her opponent was another MMA newcomer, <b>Olga</b> (5’7” / 139 lb). Both
-        girls were highly motivated to show themselves in the best possible way. They fought three rounds of four
-        minutes each, with one minute of rest between rounds. Olga was 15 lb (7 kg) heavier than Darya and had heavier
-        punches as well, so Darya took quite a beating from her. However, Darya toughed it out through all the rounds
-        and tied the match. It was a hard-won draw.
+        <Trans
+          i18nKey={`${ns}.fight2`}
+          ns="girls"
+          components={transComponents}
+          defaults={`The second time Darya fought in MMA was in July 2012 when she participated in the Nevskaya Arena Tournament organized by the club. In that fight, her opponent was another MMA newcomer, <b>Olga</b> (5'7" / 139 lb). Both girls were highly motivated to show themselves in the best possible way. They fought three rounds of four minutes each, with one minute of rest between rounds. Olga was 15 lb (7 kg) heavier than Darya and had heavier punches as well, so Darya took quite a beating from her. However, Darya toughed it out through all the rounds and tied the match. It was a hard-won draw.`}
+        />
       </p>
       <ButtonGallery images={DIR_NEVSKAYA_ARENA_2} buttonName={buttonText}/>
-      <h4><u>Results for MMA fight 2012</u></h4>
-      <p>Win-loss-draw record – 0:1:1 (as of July 1, 2012)</p>
+      <h4><u>{t(`${ns}.statsSection`, {defaultValue: 'Results for MMA fight 2012'})}</u></h4>
+      <p>{t(`${ns}.stats`, {defaultValue: 'Win-loss-draw record – 0:1:1 (as of July 1, 2012)'})}</p>
       <hr/>
       <p>
-        In our store on <a href="https://www.patreon.com/namazon/shop">Patreon</a>, you can purchase full videos of Darya’s
-        MMA fights held at the Namazon Club (Videos 18 and 21).
+        <Trans
+          i18nKey={`${ns}.store`}
+          ns="girls"
+          components={transComponents}
+          defaults={`In our store on <linkPatreon>Patreon</linkPatreon>, you can purchase full videos of Darya's MMA fights held at the Namazon Club (Videos 18 and 21).`}
+        />
       </p>
     </>
   )
--- src/data/girls-description/kara_tellerDesc.tsx
+++ src/data/girls-description/kara_tellerDesc.tsx
@@ -1,24 +1,39 @@
+'use client'
+
 import imgMain from 'public/assets/img/girl/kara-teller/k01.jpg'
 import imgMain2 from 'public/assets/img/videoGallery/09/mma-rozhdestvenskij-kubok-2012-1/01.jpg'
 import imgMain3 from 'public/assets/img/videoGallery/17/mma-kara-svetlany-2012-1/01.jpg'
 import imgMain4 from 'public/assets/img/videoGallery/18/mma-sportivnyj-prazdnik-2012-1/01.jpg'
 import imgMain5 from 'public/assets/img/videoGallery/17/mma-kara-svetlany-2012-3/9.jpg'
-import cn from "classnames"
-import {Col} from "react-bootstrap"
+import cn from 'classnames'
+import {Col} from 'react-bootstrap'
 import sC from '@/common/styles.module.scss'
-import Image from "next/image"
-import ButtonGallery from "@/common/ButtonGallery/ButtonGallery"
+import Image from 'next/image'
+import ButtonGallery from '@/common/ButtonGallery/ButtonGallery'
 import {
   DIR09_1,
   DIR_KARA_SVETLANY_1,
   DIR_KARA_SVETLANY_3,
   DIR_KARA_TELLER,
-  DIR_MMA_SPORT_HOLIDAY_1
-} from "@/common/constants/ImageContexts"
-import Link from "next/link"
+  DIR_MMA_SPORT_HOLIDAY_1,
+} from '@/common/constants/ImageContexts'
+import Link from 'next/link'
+import {Trans, useTranslation} from 'react-i18next'
+
+const transComponents = {
+  b: <b />,
+  br: <br />,
+  linkElena: <Link href="/girls/elena_vasileva2013" />,
+  linkDarya: <Link href="/girls/darya_balina" />,
+  linkPatreon: (
+    <a href="https://www.patreon.com/namazon/shop" target="_blank" rel="noopener noreferrer" />
+  ),
+}
 
 export const kara_tellerDesc = () => {
-  const buttonText = 'Other photos'
+  const {t} = useTranslation('girls')
+  const ns = 'descriptions.kara_teller'
+  const buttonText = t(`${ns}.otherPhotos`, {defaultValue: 'Other photos'})
 
   return (
     <>
@@ -27,73 +42,103 @@ export const kara_tellerDesc = () => {
       </Col>
       <ButtonGallery images={DIR_KARA_TELLER} imagesSize={{width: 330, height: 220}} buttonName={buttonText}/>
       <p>
-        <b>Kara Teller</b> (the Vityaz Club, Moscow) is a very tender, feminine girl in daily life and a tough, brave,
-        stubborn fighter on a wrestling mat and in a ring. She specializes in karate, sambo, MMA, and hand-to-hand
-        combat known as street fighting. At 5'7" and weighing 121 lb, she fears nobody and valiantly fights against
-        heavier and stronger opponents. At the Namazon Club, she mainly participated in MMA fights.
+        <Trans
+          i18nKey={`${ns}.intro`}
+          ns="girls"
+          components={transComponents}
+          defaults={`<b>Kara Teller</b> (the Vityaz Club, Moscow) is a very tender, feminine girl in daily life and a tough, brave, stubborn fighter on a wrestling mat and in a ring. She specializes in karate, sambo, MMA, and hand-to-hand combat known as street fighting. At 5'7" and weighing 121 lb, she fears nobody and valiantly fights against heavier and stronger opponents. At the Namazon Club, she mainly participated in MMA fights.`}
+        />
       </p>
-      <h4><u>Kara Teller in MMA fights</u></h4>
-      <h4>Kara vs Elena Vasilyeva. Christmas Cup 2012</h4>
+      <h4><u>{t(`${ns}.mmaSection`, {defaultValue: 'Kara Teller in MMA fights'})}</u></h4>
+      <h4>{t(`${ns}.elenaTitle`, {defaultValue: 'Kara vs Elena Vasilyeva. Christmas Cup 2012'})}</h4>
       <Image className={sC.MainImg} src={imgMain2} alt="imgMain2" />
       <p>
-        This was an intense fight with numerous punches, kicks, throws, and ground wrestling. The match had three
-        four-minute rounds with four-minute breaks between each round. <Link
-        href="/girls/elena_vasileva2013">Elena</Link> (St. Petersburg, 5'6" / 132 lb) dominated the
-        first two rounds, giving Kara a very hard time.
+        <Trans
+          i18nKey={`${ns}.elenaP1`}
+          ns="girls"
+          components={transComponents}
+          defaults={`This was an intense fight with numerous punches, kicks, throws, and ground wrestling. The match had three four-minute rounds with four-minute breaks between each round. <linkElena>Elena</linkElena> (St. Petersburg, 5'6" / 132 lb) dominated the first two rounds, giving Kara a very hard time.`}
+        />
       </p>
       <ButtonGallery images={DIR09_1} buttonName={buttonText}/>
       <p>
-        In the third round, it became clear that Elena was getting tired. Kara managed to gain control of the fight due
-        to her combative spirit and eventually won by applying a choke hold. It was a very hard and dramatic fight,
-        perhaps one of the best we've ever had at the Club.
+        {t(`${ns}.elenaP2`, {
+          defaultValue:
+            'In the third round, it became clear that Elena was getting tired. Kara managed to gain control of the fight due to her combative spirit and eventually won by applying a choke hold.',
+        })}
       </p>
-      <h4>Kara vs Svetlana Solovyova. 2012</h4>
+      <p>
+        {t(`${ns}.elenaP3`, {
+          defaultValue:
+            'It was a very hard and dramatic fight, perhaps one of the best we\'ve ever had at the Club.',
+        })}
+      </p>
+      <h4>{t(`${ns}.svetlanaTitle`, {defaultValue: 'Kara vs Svetlana Solovyova. 2012'})}</h4>
       <Image className={sC.MainImg} src={imgMain3} alt="imgMain3"/>
       <p>
-        <b>Svetlana Solovyova</b> (St. Petersburg, 5'7" / 124 lb) was clearly superior to Kara in physical power. For
-        example, her hand grip strength was measured at 56.3 kg (124 lb), more than her own weight! However, Kara’s
-        fighting experience compensated for her lack of strength during their fight.
+        <Trans
+          i18nKey={`${ns}.svetlanaP1`}
+          ns="girls"
+          components={transComponents}
+          defaults={`<b>Svetlana Solovyova</b> (St. Petersburg, 5'7" / 124 lb) was clearly superior to Kara in physical power. For example, her hand grip strength was measured at 56.3 kg (124 lb), more than her own weight! However, Kara's fighting experience compensated for her lack of strength during their fight.`}
+        />
       </p>
       <p>
-        Initially, Svetlana could withstand Kara's attacks during their stand-up exchanges, but when the fight went to
-        the ground, Kara's advantage became evident. Kara won by a choke hold in 1:13.
+        {t(`${ns}.svetlanaP2`, {
+          defaultValue:
+            'Initially, Svetlana could withstand Kara\'s attacks during their stand-up exchanges, but when the fight went to the ground, Kara\'s advantage became evident. Kara won by a choke hold in 1:13.',
+        })}
       </p>
       <ButtonGallery images={DIR_KARA_SVETLANY_1} buttonName={buttonText}/>
-      <h4>Kara vs Darya Balina. 2012</h4>
+      <h4>{t(`${ns}.daryaTitle`, {defaultValue: 'Kara vs Darya Balina. 2012'})}</h4>
       <Image className={sC.MainImg} src={imgMain4} alt="imgMain"/>
       <p>
-        Kara was the favorite to win this fight against <Link href="/girls/darya_balina">Darya
-        Balina</Link> (the female self-defense Club "Panther", St. Petersburg, 5'4" / 124 lb). While Darya had good
-        kickboxing skills, she was less confident in wrestling due to her combat training focus on self-defense. The
-        fight format was three four-minute rounds with one-minute rest periods between them.
+        <Trans
+          i18nKey={`${ns}.daryaP1`}
+          ns="girls"
+          components={transComponents}
+          defaults={`Kara was the favorite to win this fight against <linkDarya>Darya Balina</linkDarya> (the female self-defense Club "Panther", St. Petersburg, 5'4" / 124 lb). While Darya had good kickboxing skills, she was less confident in wrestling due to her combat training focus on self-defense. The fight format was three four-minute rounds with one-minute rest periods between them.`}
+        />
       </p>
       <ButtonGallery images={DIR_MMA_SPORT_HOLIDAY_1} buttonName={buttonText} />
       <p>
-        It took Kara only two rounds to secure victory. The first round was fought in stand-up, with Kara showing
-        superior striking. In the second round, the fight went to the ground where Kara again took advantage and
-        finished Darya with a choke hold, a method that has become Kara's trademark.
+        <Trans
+          i18nKey={`${ns}.daryaP2`}
+          ns="girls"
+          components={transComponents}
+          defaults={`It took Kara only two rounds to secure victory. The first round was fought in stand-up, with Kara showing superior striking. In the second round, the fight went to the ground where Kara again took advantage and finished Darya with a choke hold, a method that has become Kara's trademark.`}
+        />
       </p>
-      <h4><u>Kara Teller in Submission Grappling Fights</u></h4>
-      <h4>Kara vs Svetlana Solovyova. 2012</h4>
+      <h4>
+        <u>{t(`${ns}.sgSection`, {defaultValue: 'Kara Teller in Submission Grappling Fights'})}</u>
+      </h4>
+      <h4>{t(`${ns}.sgSvetlanaTitle`, {defaultValue: 'Kara vs Svetlana Solovyova. 2012'})}</h4>
       <Image className={sC.MainImg} src={imgMain5} alt="imgMain"/>
       <p>
-        The girls had two fights with each other. The first was a short MMA fight lasting just over a minute. Afterward,
-        they decided to test their strengths again under submission grappling rules, with a set time limit.
+        {t(`${ns}.sgP1`, {
+          defaultValue:
+            'The girls had two fights with each other. The first was a short MMA fight lasting just over a minute. Afterward, they decided to test their strengths again under submission grappling rules, with a set time limit.',
+        })}
       </p>
-      <ButtonGallery images={DIR_KARA_SVETLANY_3}  buttonName={buttonText}/>
+      <ButtonGallery images={DIR_KARA_SVETLANY_3} buttonName={buttonText}/>
       <p>
-        Both were in the same weight class, making their contest interesting and intriguing. Svetlana (St. Petersburg,
-        5'7" / 124 lb) was physically more powerful and spent most of the fight on top of Kara. Kara, on her back, used
-        a head scissors in defense and struggled to control Svetlana’s arm from the bottom, attempting a submission
-        hold. However, neither could secure a victory within the time limit. Svetlana's lack of wrestling skill and
-        Kara’s insufficient strength for a successful counterattack led to a <b>draw</b>.
+        <Trans
+          i18nKey={`${ns}.sgP2`}
+          ns="girls"
+          components={transComponents}
+          defaults={`Both were in the same weight class, making their contest interesting and intriguing. Svetlana (St. Petersburg, 5'7" / 124 lb) was physically more powerful and spent most of the fight on top of Kara. Kara, on her back, used a head scissors in defense and struggled to control Svetlana's arm from the bottom, attempting a submission hold. However, neither could secure a victory within the time limit. Svetlana's lack of wrestling skill and Kara's insufficient strength for a successful counterattack led to a <b>draw</b>.`}
+        />
       </p>
-      <h4><u>Results for 2012 MMA Tournaments</u></h4>
-      <p>Win-loss-draw record: 4:3:0 (as of July 1, 2012)</p>
+      <h4><u>{t(`${ns}.statsSection`, {defaultValue: 'Results for 2012 MMA Tournaments'})}</u></h4>
+      <p>{t(`${ns}.stats`, {defaultValue: 'Win-loss-draw record: 4:3:0 (as of July 1, 2012)'})}</p>
       <hr/>
       <p>
-        In our store on <a href="https://www.patreon.com/namazon/shop">Patreon</a>, you can purchase full videos of Kara’s
-        MMA fights held at the Namazon Club (Videos 9, 17, 18, and 20).
+        <Trans
+          i18nKey={`${ns}.store`}
+          ns="girls"
+          components={transComponents}
+          defaults={`In our store on <linkPatreon>Patreon</linkPatreon>, you can purchase full videos of Kara's MMA fights held at the Namazon Club (Videos 9, 17, 18, and 20).`}
+        />
       </p>
     </>
   )
