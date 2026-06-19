8d09b95a0dfbc730f7a175ad0f6487979aa8f5cd
Braivs <braivs@proton.me>
2026-06-19 20:12:29 +0700

    Add dark theme.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700;800&family=Days+One&display=swap"
           rel="stylesheet"
         />
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#0f0e1a" media="(prefers-color-scheme: dark)" />
       </Head>
       <body>
         <Main />
